37702ecfd88050e71238c6cfcfebc4ed0d9d405f
Reshma <nitu.kadam@gmail.com>
2023-05-02 13:09:56 +0530

    India

--- Script1.sh
+++ Script1.sh
@@ -3,10 +3,4 @@
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
 echo "Welcome to Young Minds"
-echo "Best class of AWS/DevOps"
-echo "We are learning git"
-echo "We are done with git"
-echo "This example is of branching strategy"
-echo "Hello Batch-16, We are learning Git/Github"
-
 
